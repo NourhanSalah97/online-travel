e36d5ca73db7ec1ce11b844f98b8d535ace182fd
NourhanSalah97 <norahansalah1997@gmail.com>
2023-06-07 14:26:36 +0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { FlightResultComponent } from './components/flight-result/flight-result.
 import { SelectedFlightComponent } from './bonus/selected-flight/selected-flight.component';
 import { FilterComponent } from './components/flight-result/filter/filter.component';
 import { FlightCardComponent } from './components/flight-result/flight-card/flight-card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/material.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -20,7 +23,10 @@ import { FlightCardComponent } from './components/flight-result/flight-card/flig
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button'
+
+
+
+
+
+@NgModule({
+    declarations: [],
+    exports: [
+        MatButtonModule,
+    ] 
+})
+export class MaterialModule { }
